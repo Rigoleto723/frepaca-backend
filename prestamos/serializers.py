49578cd36101d386e3dd4c333f9cf2d308db509e
Rigoleto723 <ivanvargasc723@gmail.com>
2025-04-08 21:49:34 -0500

    Se adiciona la informacion del cliente en el serializados de prestamos

--- prestamos/serializers.py
+++ prestamos/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Prestamo
+from clientes.serializers import ClienteSerializer
 
 class PrestamoSerializer(serializers.ModelSerializer):
+    cliente = ClienteSerializer(read_only=True)
+
     class Meta:
         model = Prestamo
         fields = '__all__'
