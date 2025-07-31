3e845bcd60cf64c19496a359d6d375a0fecd0fc3
Ivan Vargas <ivanvargasc723@gmail.com>
2025-07-30 23:16:15 -0500

    Cambio logica de creacion de usuario admin y actualizcion archivo requitements.txt

--- authentication/signals.py
+++ authentication/signals.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Group
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
+from .models import CustomUser
 
 @receiver(post_migrate)
 def create_default_groups(sender, **kwargs):
@@ -9,3 +10,11 @@ def create_default_groups(sender, **kwargs):
     for group_name in groups:
         Group.objects.get_or_create(name=group_name)
     print("✅ Grupos creados exitosamente.")
+
+@receiver(post_save, sender=CustomUser)
+def add_superuser_to_admin_group(sender, instance, created, **kwargs):
+    """Agrega automáticamente el superusuario al grupo 'admin'."""
+    if created and instance.is_superuser:
+        admin_group, _ = Group.objects.get_or_create(name="admin")
+        instance.groups.add(admin_group)
+        print(f"✅ Usuario {instance.username} agregado al grupo 'admin'.")
